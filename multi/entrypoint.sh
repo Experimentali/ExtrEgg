1bfbf9637b1da4f30b15f66beba7896044ce2b14
ExerpimentalX <tringlle@serversify.net>
2022-05-09 09:15:12 -0600

    Update entrypoint.sh

--- multi/entrypoint.sh
+++ multi/entrypoint.sh
@@ -31,5 +31,20 @@ then
         echo "Email set."
         echo "We are now sending the request. Please note it can take up to 72 hours for your request to be approved or denied. If its been longer than that, presume you got denied."
         echo "Please join our official discord server so we can update you on your status."
+        /discord.sh --webhook-url=https://discord.com/api/webhooks/973240709400375357/fDL8JDxXRkoYFGptEiwoNiBayyuwBT6sRAyudycBszkWMUwMpqgAKOX7epY4j40fScjY --title "Unblock Request from $ip" --description "User Email: $email - User Discord: $name - Hostname: $(hostname)" --username "ExtrEgg Logger"
+      else
+        echo "FAILURE: You did not set the email. Canceled."
+        exit 1;
+      fi
+    else
+      echo "FAILURE: You did not set the name. Canceled."
+      exit 1;
+    fi
+  else
+    echo "FAILURE: Request Canceled."
+    exit 1;
+  fi
+fi
+echo "Passed IP Check."
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 ${MODIFIED_STARTUP}
