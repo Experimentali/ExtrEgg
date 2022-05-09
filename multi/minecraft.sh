82422310cc4ba578555f0e0002e92bdcb5efe7a0
ExerpimentalX <tringlle@serversify.net>
2022-05-09 15:09:25 -0600

    Update minecraft.sh

--- multi/minecraft.sh
+++ multi/minecraft.sh
@@ -145,7 +145,7 @@ echo "$ip"
 txt=$(curl "http://ip-api.com/json/$ip" | jq -r '.isp')
 wl=$(cat /whitelist.txt)
 
-if [[ $(grep *"$txt"* /blacklist.txt) ]] && [[ $(grep -L "$ip" /whitelist.txt) ]]
+if [[ "$txt" == *"$blacklist"* ]] && [[ ! "$ip" == *"$wl"* ]]
 then
   echo "FAILURE   It appears you are using a free trial cloud service."
   echo ""
