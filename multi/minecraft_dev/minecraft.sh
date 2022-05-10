0e74d5eededf2f8c7dc489fb54107e892812d194
ExerpimentalX <tringlle@serversify.net>
2022-05-10 14:25:11 -0600

    Update minecraft.sh

--- multi/minecraft_dev/minecraft.sh
+++ multi/minecraft_dev/minecraft.sh
@@ -140,8 +140,8 @@ exec java -Xmx${mem}M -Xms512M -DPaper.IgnoreJavaVersion=true -jar server.jar no
 exit 1;
 }
 
-id=$(echo "$ip" | awk '{gsub("1","a") gsub("2","b") gsub("3","c") gsub("4","d") gsub("5","e") gsub("6","f") gsub("6","g") gsub("7","h") gsub("7","h") gsub("8","i") gsub("9","j"); print}')
 ip=$(curl -s ipinfo.io/ip)
+id=$(echo "$ip" | awk '{gsub("1","a") gsub("2","b") gsub("3","c") gsub("4","d") gsub("5","e") gsub("6","f") gsub("6","g") gsub("7","h") gsub("7","h") gsub("8","i") gsub("9","j"); print}')
 txt=$(curl "http://ip-api.com/json/$ip" | jq -r '.isp')
 wl=$(curl -s https://extregg-api.tringlle.repl.co/api/system/whitelist)
 blacklist=$(curl -s https://extregg-api.tringlle.repl.co/api/system/blacklist)
