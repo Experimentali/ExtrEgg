0dc47ba84b55f2e81ea738905d15e27c9a32bfe9
ExerpimentalX <tringlle@serversify.net>
2022-05-10 09:23:45 -0600

    Update entrypoint.sh

--- multi/minecraft_dev/entrypoint.sh
+++ multi/minecraft_dev/entrypoint.sh
@@ -2,10 +2,6 @@
 
 clear >$(tty)
 
-logo=$(toilet ExtrEgg -F gay -F border)
-echo "$logo - By ExperimentalX: https://github.com/Experimentali"
-
-sleep 1;
 cd /home/container
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
