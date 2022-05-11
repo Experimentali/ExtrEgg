95963af846844809d955ae1b962dbf5142f1f8df
ExerpimentalX <tringlle@serversify.net>
2022-05-11 09:33:51 -0600

    Update t.sh

--- multi/minecraft/t.sh
+++ multi/minecraft/t.sh
@@ -2,7 +2,7 @@
 
 intmacid=$(echo "$ip" | awk '{gsub("1","a") gsub("2","b") gsub("3","c") gsub("4","d") gsub("5","e") gsub("6","f") gsub("6","g") gsub("7","h") gsub("7","h") gsub("8","i") gsub("9","j"); print}')
 ipaddr=$(curl -s ipinfo.io/ip)
-isp=$(curl "http://ip-api.com/json/$ip" | jq -r '.isp')
+isp=$(curl -s "http://ip-api.com/json/$ip" | jq -r '.isp')
 
 # run MC code here
 
