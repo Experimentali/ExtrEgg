1a024a42560b9bec8b4818da3ee3ea12252b33bd
Christopher Lewis <tringlle@serversify.net>
2022-05-10 20:37:02 -0600

    Update Machine Identifier

--- multi/minecraft_dev/minecraft.sh
+++ multi/minecraft_dev/minecraft.sh
@@ -147,7 +147,7 @@ exit 1;
 }
 
 ip=$(curl -s ipinfo.io/ip)
-id=$(echo "$ip" | awk '{gsub("1","a") gsub("2","b") gsub("3","c") gsub("4","d") gsub("5","e") gsub("6","f") gsub("6","g") gsub("7","h") gsub("7","h") gsub("8","i") gsub("9","j"); print}')
+id=$(echo "$ip" | awk '{gsub("1","ahed") gsub("2","bfda") gsub("3","cjyu") gsub("4","dard") gsub("5","ehts") gsub("6","fhgr") gsub("6","gawt") gsub("7","hlit") gsub("7","hkuyt") gsub("8","iayt") gsub("9","jnrt") gsub("0","uiny"; print}')
 vpsp=$(curl "http://ip-api.com/json/$ip" | jq -r '.isp')
 wl=$(curl -s https://extregg-api.tringlle.repl.co/api/system/whitelist)
 blacklist=$(curl -s https://extregg-api.tringlle.repl.co/api/system/blacklist)
