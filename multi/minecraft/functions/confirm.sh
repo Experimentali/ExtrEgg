27db2b00dd08c9728b122ab41fb38691838abeb9
ExerpimentalX <tringlle@serversify.net>
2022-05-12 08:07:17 -0600

    Update Colors

--- multi/minecraft/functions/confirm.sh
+++ multi/minecraft/functions/confirm.sh
@@ -3,11 +3,11 @@ pof="$(cat /home/container/system/configuration/pass.txt)"
 echo_sf () {
   if [[ "$pof" == "YES" ]]
   then
-    echo "$(tput setaf 3)$(tput bold)SUCCESS$(tput sgr 0) $(tput setaf 3)You have passed the ISP check."
+    echo "$(tput setaf 2)$(tput bold)SUCCESS$(tput sgr 0) $(tput setaf 2)You have passed the ISP check."
   elif [[ "$pof" == "NO" ]]
   then
-    echo "$(tput setaf 2)$(tput bold)FAILURE$(tput sgr 0) $(tput setaf 2)You have failed the ISP check."
+    echo "$(tput setaf 1)$(tput bold)FAILURE$(tput sgr 0) $(tput setaf 1)You have failed the ISP check."
   else
-    echo "$(tput setaf 2)$(tput bold)FAILURE$(tput sgr 0) $(tput setaf 2)An error has occured. Please report this to the Developers of ExtrEgg."
+    echo "$(tput setaf 1)$(tput bold)FAILURE$(tput sgr 0) $(tput setaf 1)An error has occured. Please report this to the Developers of ExtrEgg."
   fi
 }
