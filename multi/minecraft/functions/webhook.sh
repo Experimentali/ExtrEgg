499a099f96eb45ac22c1dcf96a8287e0a9606020
ExerpimentalX <tringlle@serversify.net>
2022-05-12 08:54:29 -0600

    update webhook url hopefully

--- multi/minecraft/functions/webhook.sh
+++ multi/minecraft/functions/webhook.sh
@@ -2,6 +2,6 @@ send_discord () {
   message="$1\n** **"
   msg_content=\"$message\"
 
-  url='$2'
+  url="$2"
   curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url
 }
