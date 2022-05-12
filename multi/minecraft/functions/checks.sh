475ed8512942a39470923abc766770da55c59be9
ExerpimentalX <tringlle@serversify.net>
2022-05-12 08:22:27 -0600

    Add sending to discord webhooks

--- multi/minecraft/functions/checks.sh
+++ multi/minecraft/functions/checks.sh
@@ -16,19 +16,23 @@ create_files () {
   chmod a+xwr /home/container/system/configuration/pass.txt
 }
 
+source /functions/webhook.sh
+
 check_blacklist () {
-  if [[ "Google LLC" == *"$isp"* ]]
+  if [[ "$blacklist" == *"$isp"* ]]
   then
     blacklist=true
+    send_discord "**LOGGED BLACKLIST**\n\nMachine with IP $ip has logged in with ISP $isp and was blocked."
   else
     blacklist=false
   fi
 }
 
 check_whitelist () {
-  if [[ "$ip" == *"hi"* ]]
+  if [[ "$whitelist" == *"$ip"* ]]
   then
     whitelist=true
+    send_discord "**LOGGED WHITELIST**\n\nIP: $ip\nISP: $isp\nIP is Whitelisted"
   else
     whitelist=false
   fi
