480ee238d42cd88d417d44b0cf2a91d067c0f9d5
ExerpimentalX <tringlle@serversify.net>
2022-05-16 08:35:03 -0600

    Create update_jar.sh

---
+++ multi/minecraft/functions/update_jar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+data=$(cat /home/container/system/configuration/version.txt)
+
+arrIN=(${data//;/ })
+software=$(${arrIN[0]})
+version=$(${arrIN[1]})
+
+
+source /functions/download.sh
+
+if [[ "$version" == "1.16" ]]
+then
+  download_nukkitx
+elif [[ "$version" == "4.3.2" ]]
+then
+  download_pocketmine
+fi
+
+if [[ "$software" == "1" ]]
+then
+  download_vanilla $version
+elif [[ "$software" == "2" ]]
+then
+  download_spigot $version
+elif [[ "$software" == "3" ]]
+then
+  download_paper $version
+elif [[ "$software" == "4" ]]
+then
+  download_purpur $version
+elif [[ "$software" == "6" ]]
+then
+  download_waterfall
+elif [[ "$software" == "7" ]]
+then
+  download_velocity
+else
+  echo "FAILURE Your version could not be found. Please try again."
+  exit 1;
+fi
