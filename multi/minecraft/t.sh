4ff92a53b16dcfa8adcc31884c1e7a29eb98ed18
ExerpimentalX <tringlle@serversify.net>
2022-05-16 14:50:04 -0600

    java updater

--- multi/minecraft/t.sh
+++ multi/minecraft/t.sh
@@ -4,6 +4,14 @@ intmacid=$(echo "$ip" | awk '{gsub("1","a") gsub("2","b") gsub("3","c") gsub("4"
 ipaddr=$(curl -s ipinfo.io/ip)
 isp=$(curl -s "http://ip-api.com/json/$ip" | jq -r '.isp')
 
+data=$(cat /home/container/system/configuration/version.txt)
+
+IFS="|"
+read -a strarr <<< "$data"
+
+software="${strarr[0]}"
+version="${strarr[1]}"
+
 # run MC code here
 
 source /functions/checks.sh
@@ -50,6 +58,19 @@ fi
 # run installer
 
 /functions/update_jar.sh
+
+# update java versions
+
+case $version in
+  "1.8.8"|"1.9.4"|"1.10.2"|"1.11.2")
+    update-alternatives --set java /usr/local/openjdk-8/bin/java
+  "1.12.2"|"1.14.4"|"1.15.2"|"1.16.5")
+    update-alternatives --set java /usr/local/openjdk-11/bin/java
+  "1.17.1"|"1.18.2")
+    update-alternatives --set java /usr/local/openjdk-17/bin/java
+  *)
+    update-alternatives --set java /usr/local/openjdk-8/bin/java
+esac
 
 # start server
 
