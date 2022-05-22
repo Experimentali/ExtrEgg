09051032c492006bc37451e41178b321e798353d
Christopher Lewis <tringlle@serversify.net>
2022-05-21 20:16:15 -0600

    java version fixer thinger

--- multi/minecraft/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-title=$(toilet ExtrEgg -F gay -F border)
-echo "$title - By ExperimentalX"
-
-sleep 1;
-
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-${MODIFIED_STARTUP}
