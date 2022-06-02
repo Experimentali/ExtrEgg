bab6a76e760bfea3a57ca8c9ecbc95d9e47c3471
Christopher Lewis <tringlle@serversify.net>
2022-06-02 14:19:57 -0600

    Testing my script

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ echo 'PUSH TO GITHUB'
 sleep 1;
 
 echo 'REOBFUSCATING'
-obfuscator/auto.sh
+./auto.sh
 
 read -p "What is your commit message: " msg
 read -p 'Are you sure you want to push your commits to GitHub?' y
